b31126a1397503b8f40789c307961b078efe0885
e1b18074 <e1b18074@st.oit.ac.jp>
2020-10-13 00:19:24 +0900

    じゃんけん実装

--- src/main/java/controller/Lec02Controller.java
+++ src/main/java/controller/Lec02Controller.java
@@ -9,6 +9,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Lec02Controller {
+  @GetMapping("/lec02")
+    public String lec02(@RequestParam Integer hand,ModelMap model){
+      String Hand;
+      String CPU="相手の手 Gu";
+      String kekka;
+      if(hand == 0){
+      Hand="あなたの手 Gu";
+      kekka="Aiko";
+      model.addAttribute("Hand",Hand);
+      model.addAttribute("CPU",CPU);
+      model.addAttribute("kekka",kekka)
+      ;
+      }
+      if(hand == 1){
+      Hand="あなたの手 Choki";
+      kekka="You Lose!";
+      model.addAttribute("Hand",Hand);
+      model.addAttribute("CPU",CPU);
+      model.addAttribute("kekka",kekka);
+      }
+      if(hand == 2){
+      Hand="あなたの手 Pa";
+      kekka="You Win!";
+      model.addAttribute("Hand",Hand);
+      model.addAttribute("CPU",CPU);
+      model.addAttribute("kekka",kekka);
+      }
+      return "lec02.html";
+    }
+
   @PostMapping("/lec02")
   public String lec02(@RequestParam String username,ModelMap model) {
     model.addAttribute("username", username);
